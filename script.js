1f00d782b1f9f6b33ab7766872c775b132cab02a
Rafael Oliveira <rafa.celos.dev@gmail.com>
2022-10-28 05:08:10 +0100

    new button

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let isIgnite = false
-function changeCard(event) {
-  const card = event.currentTarget
+// function changeCard(event) {
+//   const card = event.currentTarget
+//   const bg = isIgnite ? "explorer" : "ignite"
+//   isIgnite = !isIgnite
+//   card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
+// }
+
+
+let swapTrack = document.querySelector("#choseTrack");
+
+swapTrack.addEventListener("click", function(){
+  const card = document.querySelector(".card")
   const bg = isIgnite ? "explorer" : "ignite"
   isIgnite = !isIgnite
   card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
-}
+});
